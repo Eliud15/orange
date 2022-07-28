353c1fbae7a49df606fd3a417c51e6f85a139f7a
Eliuth E'p <98053715+Eliud15@users.noreply.github.com>
2022-07-27 19:23:21 -0500

    Add files via upload

---
+++ friendzone.js
@@ -0,0 +1,41 @@
+var enviar=document.querySelector("#enviar");
+var Actualizar=document.querySelector("#actualizar");
+var usuario=document.querySelector("#usuario");
+var cancelar=document.querySelector(".cancelar");
+var aceptar=document.querySelector(".aceptar");
+usuario.addEventListener("click",()=>{
+     var salir=document.querySelector(".salir")
+    salir.classList.add('aparecer')
+    salir.style.visibility="visible"
+});
+enviar.addEventListener("click",(e)=>{
+   
+    var usuario=document.querySelector("#usuario");
+    var nombre=document.querySelector("#nombre");
+
+    var mensaje=document.querySelector("#mensaje");
+    if (mensaje.value.length<=0) {
+        e.preventDefault();
+        alert("No puedes dejar el mensaje en blanco")
+    }
+    else if(mensaje.value.length>=9000){
+        e.preventDefault();
+        alert("No puedes Escribir mas de 9.000 caracteres en el mensaje")
+    }
+    else{
+        nombre.value=usuario.innerHTML
+    }
+});
+
+cancelar.addEventListener("click",(e)=>{
+    var salir=document.querySelector(".salir")
+    salir.classList.remove('aparecer')
+    salir.style.visibility="hidden";
+    
+});
+aceptar.addEventListener("click",(e)=>{
+    location.href="salir.php"
+});
+Actualizar.addEventListener("click",(e)=>{
+    location.reload("")
+});
